0108cafc501c306f1afdcaa9ffc74f7354f3e7ea
Lalith Yagnavalkya <lalluyagnavalkya.t@gmail.com>
2022-04-19 15:20:08 +0530

    add Header

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 //react router dom
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 //pages
 import {
   About,
@@ -12,24 +12,24 @@ import {
   ProductDetails,
   Products,
 } from "./pages";
+
 //components
+import Header from "./components/Header";
+
 export default function App() {
   return (
     <Router>
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route path="/about">
-          <About />
-        </Route>
-        <Route path="/cart">
-          <Cart />
-        </Route>
-        <Route path="*">
-          <Error />
-        </Route>
-      </Switch>
+      <Header />
+      <Routes>
+        <Route exact path="/" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/cart" element={<Cart />}></Route>
+        <Route path="/checkout" element={<Checkout />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route exact path="/products" element={<Products />}></Route>
+        <Route path="/products/:id" element={<ProductDetails />}></Route>
+        <Route path="*" element={Error}></Route>
+      </Routes>
     </Router>
   );
 }
