672c3162a413620e18f814e951df83d998c6aa51
Lalith Yagnavalkya <lalluyagnavalkya.t@gmail.com>
2022-04-19 12:52:56 +0530

    router setup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,35 @@
 import React from "react";
-
+//react router dom
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+//pages
+import {
+  About,
+  Cart,
+  Checkout,
+  Error,
+  Home,
+  Login,
+  ProductDetails,
+  Products,
+} from "./pages";
+//components
 export default function App() {
-  return <h1>hello from app component</h1>;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/cart">
+          <Cart />
+        </Route>
+        <Route path="*">
+          <Error />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
